03671be40086a28552d13c9b6e2f5fa44b37bfa4
Luis David <david12.29@hotmail.com>
2024-11-19 09:16:32 -0500

    Cree la estructura basica del ejemplo

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <TodoList />
+      <TodoCounter />
+      <TodoSearch />
+      <TodoList>
+        <TodoList />
+      </TodoList>
+      <CreateTodoButton />
     </div>
   );
 }
