dafc93f9f2bf7fb3b2dcc9be628cc3cab50255b7
Luis David <david12.29@hotmail.com>
2024-11-21 11:04:00 -0500

    Explicacion de onChange

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,6 +1,17 @@
 import "./TodoSearch.css";
 function TodoSearch() {
-  return <input placeholder="Escribe aqui" className="TodoSearch" />;
+  return (
+    <input
+      placeholder="Escribe aqui"
+      className="TodoSearch"
+      onChange={(event) => {
+        console.log("Escribiste en mi el TodoSearch");
+        console.log(event);
+        console.log(event.target);
+        console.log(event.target.value);
+      }}
+    />
+  );
 }
 
 export { TodoSearch };
