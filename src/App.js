1e5ae3ca80481af5117d61963da039450b3c738d
Luis David <david12.29@hotmail.com>
2024-11-21 11:43:13 -0500

    Explicacion de estados derivados

--- src/App.js
+++ src/App.js
@@ -9,17 +9,19 @@ import React from "react";
 const defaultTodos = [
   { text: "Actividad1 ", completed: true },
   { text: "Actividad2 ", completed: false },
-  { text: "Actividad3 ", completed: false },
+  { text: "Actividad3 ", completed: true },
   { text: "Actividad4 ", completed: false },
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
   const [searchValue, setSearchValue] = React.useState("");
 
+  const completedTodos = todos.filter((todo) => todo.completed).length;
   console.log("Mira los cambios de searchValue: " + searchValue);
   return (
     <React.Fragment>
-      <TodoCounter completed={16} total={25} />
+      <TodoCounter completed={completedTodos} total={25} />
       <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
       <TodoList>
         {defaultTodos.map((todo) => (
