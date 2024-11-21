d50ec583bf90306ca219b0df50ef45a7253f9e9d
Luis David <david12.29@hotmail.com>
2024-11-21 11:13:21 -0500

    Explicacion useState 2 en TodoSearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -4,16 +4,14 @@ import "./TodoSearch.css";
 function TodoSearch() {
   const [searchValue, setSearchValue] = React.useState("");
 
+  console.log("Mira los cambios de searchValue: " + searchValue);
   return (
     <input
       placeholder="Escribe aqui"
       className="TodoSearch"
       value={searchValue}
       onChange={(event) => {
-        console.log("Escribiste en mi el TodoSearch");
-        console.log(event);
-        console.log(event.target);
-        console.log(event.target.value);
+        setSearchValue(event.target.value);
       }}
     />
   );
