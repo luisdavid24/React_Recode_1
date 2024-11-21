ed63591558d044be812ac1de8b2249a267cffe11
Luis David <david12.29@hotmail.com>
2024-11-21 11:25:30 -0500

    Explicacion del porque usar useSate

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -18,3 +18,23 @@ function TodoSearch() {
 }
 
 export { TodoSearch };
+
+let veces = 0;
+function Explicacion() {
+  return (
+    <>
+      <p>Diste el siguiente numero de cicls{veces}</p>
+      <button onClick={() => veces++}>Click</button>
+    </>
+  );
+}
+
+function ExplicacionMejorada() {
+  let [state, setState] = React.useState(0);
+  return (
+    <>
+      <p>Diste el siguiente numero de cicls{state}</p>
+      <button onClick={() => setState(state++)}>Click</button>
+    </>
+  );
+}
