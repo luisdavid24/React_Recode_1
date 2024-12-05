500178048477520ebe0e124339f11d4b95a78173
Luis David <david12.29@hotmail.com>
2024-12-05 09:46:02 -0500

    Mejore los estilos de TodoForm

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -3,7 +3,11 @@ import "./TodoForm.css";
 
 function TodoForm() {
   return (
-    <form>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+      }}
+    >
       <label>Digita la nueva tarea</label>
       <textarea placeholder="Digita en mi" />
       <div className="TodoBtnContainer">
