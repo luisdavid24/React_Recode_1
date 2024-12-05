fcb36a54ab9e5f3b7a1f65f609f07a483da3f4d4
Luis David <david12.29@hotmail.com>
2024-12-05 11:42:07 -0500

    En el componente TodoForm agrege la funcion onCancel, onChange y agrege otro estado "newTodoValue "

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,17 +1,37 @@
 import React from "react";
 import "./TodoForm.css";
+import { TodoContext } from "../TodoContext";
 
 function TodoForm() {
+  const { setOpenModal } = React.useContext(TodoContext);
+  const [newTodoValue, setNewTodoValue] = React.useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    setOpenModal(false);
+  };
+  const onCancel = () => {
+    setOpenModal(false);
+  };
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value);
+  };
   return (
-    <form
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
-    >
+    <form onSubmit={onSubmit}>
       <label>Digita la nueva tarea</label>
-      <textarea placeholder="Digita en mi" />
+      <textarea
+        placeholder="Digita en mi"
+        onChange={onChange}
+        value={newTodoValue}
+      />
       <div className="TodoBtnContainer">
-        <button className="TodoFormBtnCancelar">Cancelar</button>
+        <button
+          className="TodoFormBtnCancelar"
+          type="button"
+          onClick={onCancel}
+        >
+          Cancelar
+        </button>
         <button className="TodoFormBtnAgregar">Añadir</button>
       </div>
     </form>
